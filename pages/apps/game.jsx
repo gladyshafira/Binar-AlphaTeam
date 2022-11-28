39369646b90b6785136d4568890204a349aae017
Hafidz Firmansyah <Hafidz.Ghufara@bfi.co.id>
2022-11-28 19:22:37 +0700

    fix router protection for game page

--- pages/apps/game.jsx
+++ pages/apps/game.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import * as Components from "./rps.styled";
 import Play from "../../src/component/game-rps/Play";
+import withAuth from "../../src/withAuth";
+import { useSelector } from "react-redux";
 
-export default function GameRps() {
+function GameRps() {
   const [myChoice, setMyChoice] = useState("");
   const [score, setScore] = useState(0);
+  const profile = useSelector((state) => state.authReducer.profile);
   return (
     <Components.Container>
       <Play setMyChoice={setMyChoice} />
     </Components.Container>
   );
 }
+export default withAuth(GameRps);
